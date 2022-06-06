ceed7e6d327e8fd50b6c55ebd3179e437d5749c6
changhle <changhle@student.42seoul.kr>
2022-06-06 16:45:30 +0900

    pisicine

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -1,25 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_is_sort.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: changhle <changhle@student.42seoul.>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/27 17:27:36 by changhle          #+#    #+#             */
-/*   Updated: 2021/09/27 18:13:09 by changhle         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_is_sort(int *tab, int length, int(*f)(int, int))
 {
 	int	index;
+	int	sort;
 
+	index = 0;
+	sort = 1;
+	while (index < length - 1 && sort == 1)
+	{
+		if (f(tab[index], tab[index + 1]) > 0)
+		{
+			sort = -1;
+			break ;
+		}
+		index++;
+	}
 	index = 0;
 	while (index < length - 1)
 	{
-		if (f(tab[0], tab[1]) > 0 && f(tab[index], tab[index + 1]) < 0)
+		if (sort == 1 && f(tab[index], tab[index + 1]) > 0)
 			return (0);
-		else if (f(tab[0], tab[1]) < 0 && f(tab[index], tab[index + 1]) > 0)
+		else if (sort == -1 && f(tab[index], tab[index + 1]) < 0)
 			return (0);
 		index++;
 	}
